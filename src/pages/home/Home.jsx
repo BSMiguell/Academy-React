1f2ef5df2f5032cfdde3e0b1820976ca463a6116
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-26 03:48:01 -0300

    Finalizando O MainHeader

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import MainHeader from "../../components/MainHeader";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <MainHeader />
+    </>
+  );
 };
-
 export default Home;
