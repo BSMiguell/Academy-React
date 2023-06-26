ae8f1edcb88c527fa3ff262c8f584e2ca7df44e2
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-26 09:47:46 -0300

    Finalizando o NotFound

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,19 @@
-import "./notfound.css";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Notfound = () => {
-  return <div>Notfound</div>;
+import "./notFound.css";
+
+const NotFound = () => {
+  return (
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
-export default Notfound;
+export default NotFound;
